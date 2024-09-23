64a1f020e3cdfe1e7e0f8a76e387431686261ecc
your name <essie.k004@gmail.com>
2024-09-23 22:57:22 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Esther Kamau';
+const welcomeMessage = 'Welcome to Flatbook,  ${Esther Kamau}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+
